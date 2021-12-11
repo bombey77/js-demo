6f1c80a1d17f5a6bf9e3d5f84c0a9020e4cd8f15
Motovilov Roman <bombey131@gmail.com>
2021-12-12 01:06:27 +0200

    Add promise status example

---
+++ examples/demo_5_promise/promise_status.js
@@ -0,0 +1,12 @@
+const allWentWell = true;
+
+const promise = new Promise((resolve, reject) => {
+    if (allWentWell) {
+        resolve('All went well!');
+    } else {
+        reject('All went not well!');
+    }
+});
+promise
+    .then(data => console.log(data))
+    .catch(err => console.error('Error', err));
