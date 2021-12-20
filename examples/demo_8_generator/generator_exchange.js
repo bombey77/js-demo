0adc06344c7de753d5db7975783c73efe0a797a0
Motovilov Roman <bombey131@gmail.com>
2021-12-20 02:27:04 +0200

    Add another generator example

--- examples/demo_8_generator/generator_exchange.js
+++ examples/demo_8_generator/generator_exchange.js
@@ -10,3 +10,22 @@ const q = it.next().value; // Итератор задаёт вопрос
 console.log(q);
 const a = it.next("John").value; // Получен ответ на вопрос
 console.log(a);
+
+
+function* increment() {
+    let id = 1;
+    while (true) {
+        const increment = yield id;
+        if (increment != null) {
+            id += increment;
+        } else {
+            id++;
+        }
+    }
+}
+const incr = increment();
+console.log(incr.next());
+console.log(incr.next());
+console.log(incr.next());
+console.log(incr.next(3));
+console.log(incr.next());
